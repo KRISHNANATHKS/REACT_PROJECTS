b38148322db8d86fb609190bdb5294ca44ba4b1f
Krishnanath K S <krishnanathks2003@gmail.com>
2026-06-07 23:51:16 +0530

    Connected frontend to deployed backend

--- proj4/app/src/App.jsx
+++ proj4/app/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components'
 import SearchResult from './SearchResult/SearchResult.jsx';
 
-export const BASE_URL="http://localhost:9000";
+export const BASE_URL="https://food-backend-8z88.onrender.com";
 
 function App() {
 
